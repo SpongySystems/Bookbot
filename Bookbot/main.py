32f91dccbedec0b18f943dc0229b88e31a88496d
Rien <rien.strootman@protonmail.com>
2024-04-10 17:43:35 +0200

    final release

--- Bookbot/main.py
+++ Bookbot/main.py
@@ -6,15 +6,17 @@ def main():
     letters = letter_count(book_content)
     generate_report(book_name, word_count, letters)
 
-
+# load the text of the file
 def get_book_text(bookpath):
     with open(bookpath) as f:
         return f.read()
 
+#count the words in the document
 def count_words(text):
     words = text.split()
     return len(words)
 
+#count only the letters in the document and store it in a dictionary
 def letter_count(text):
     letter_dict = {}
     for letter in text:
@@ -25,6 +27,7 @@ def letter_count(text):
         letter_dict[letter.lower()] += 1
     return letter_dict
 
+#generate a report of the book with the word- and letter count
 def generate_report(book_name, word_count, letters):
     letter_list = sort_dict(letters)
 
@@ -36,9 +39,11 @@ def generate_report(book_name, word_count, letters):
 
     print("---End report---")
 
+#function that handles the sorting
 def sort_on(dict):
     return dict["count"]
 
+#sort the letter count from largest to smallest count
 def sort_dict(dict):
     letter_list = []
     for char in dict:
